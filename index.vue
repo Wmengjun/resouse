11a19cbae030fbcd2b12dbc421abc5b6eba6a69a
meng <18391987550@163.com>
2018-06-05 18:35:51 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,26 +1,31 @@
 <template>
-  <div style="text-align: left;">
-    <span>关键字:</span>
-    <div v-for="(row,index) in num" style="display: inline;margin-left:5%;height:50px;">
+  <div style="text-align: left;height:100%">
+  	<div class="divtwo">
+  	<span style="display:block;margin-bottom: inherit;">关键字:</span>
+    
+    <div v-for="(row,index) in num" style="display: inline;" @mouseover="subover()" @mouseout="subout()">
       <textarea
       v-model="data[index]"
       name="name"
-      style="height:100%;"
+      style="height:5%;"
       />
-      <div style="display: inline;" @click="subkey(index)">
+      <div v-show="!subhover"style="display: inline;" @click="subkey(index)" class="subimg" >
+        <button style="height:30px;width:30px;background-color: transparent;border: none;"/>
+      </div>
+      <div v-show="subhover"style="display: inline;" @click="subkey(index)" class="subimg" >
         <img src="@/assets/sub.png" style="height:30px;width:30px">
       </div>
     </div>
 
-    <div style="display: inline;" @click="addkey()">
+    <div  style="display: inline;" @click="addkey()">
       <img src="@/assets/add.png" style="height:30px;width:30px">
     </div>
-    <div style="display: inline;" >
+    <div style="display: inline;" @click="change()">
       <img src="@/assets/right.png" style="height:30px;width:30px">
     </div>
-    <br><span >正则表达：</span>
+    <br><span style="display:block">正则表达：</span>
       <textarea class="textareastyle" v-model="regx"></textarea>
-    <br><span>命令显示：</span>
+    <br><span style="display:block">命令显示：</span>
       <textarea class="textareastyle" v-model="mmlvalue" ></textarea>
       <hr style="height:1px;border:none;border-top:1px dashed #0066CC;" />
       <br><span >&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
@@ -28,6 +33,7 @@
     <!-- <div style="border: 1px solid #d8caca;height: 100px;width:80%;margin-left:5%">
       命令生成操作区域。。。待定
     </div> -->
+    </div>
 
   </div>
 </template>
@@ -41,15 +47,31 @@ export default {
       data: [],
       regx: '',
       mmlvalue: '',
-      creat:'命令生成操作区域。。。待定'
+      creat:'命令生成操作区域。。。待定',
+      subhover: false
     }
   },
   methods: {
+  	subover (){
+  		this.subhover = true
+  	},
+  	subout (){
+  		this.subhover = false
+  	},
     addkey () {
-      this.num = this.num + 1
-    },
+    	if (this.num < 3) {
+    		this.num = this.num + 1
+    	}
+    	else {
+    		window.alert("行数已达上限")
+    	};
+      
+    	},
     subkey(index) {
-      this.num = this.num - 1
+    	if (this.num !== 1) {
+    		this.num = this.num - 1
+    	};
+      
       for(var i =0;i <this.data.length;i++){
         var temp = this.data[i];
         if(!isNaN(index)){
@@ -62,8 +84,35 @@ export default {
           this.datalength = this.data.length-1;
         }
       }
+    },
+    change(){
+    	var newdata ={}
+      var regdata = {}
+      var regxdata = {}
+      var a = "/.*\\d+.*/"
+    	var keyword = "key"
+    	for (var i = 0; i < this.data.length; i++) {
+    		newdata[keyword+i] = this.data[i].split("\n")
+    	}
+    	console.log(newdata)
+      var sumtem = ""
+    	for ( var item in newdata) {
+        var tem = ""
+    		for (var k = 0; k < newdata[item].length; k++) { 
+          if (eval(a).test(newdata[item][k])) 
+            {
+              
+            } 
+          else
+            {
+              tem = tem+newdata[item][k] +".*\\s"
+            }
+    	}
+      sumtem = sumtem + tem +"\n"
     }
+    this.regx = sumtem
   }
+}
 }
 </script>
 
@@ -75,4 +124,12 @@ export default {
   width: 80%;
   margin: 10px;
 }
+.divtwo {
+	background-color: beige;
+    margin: 10px 0px 10px 20%;
+    padding: 20px;
+    width: 60%;
+    height: 100%;
+}
+
 </style>
